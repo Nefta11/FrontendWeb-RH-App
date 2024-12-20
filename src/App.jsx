044a5e6bb821d267274ea6c9afa1d17020e38be8
Nefta11 <neftaliarturohernandez@gmail.com>
2024-12-20 14:07:04 -0600

    Agregar componente ListadoEmpleados y actualizar App.jsx para su inclusión

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
+import ListadoEmpleados from "./empleados/ListadoEmpleados";
+
 function App() {
   return (
-    <div className="container text-center mt-5"> 
-      <h2>Sistema de Recursos Humanos</h2>
-    </div>
+< ListadoEmpleados/>
   );
 }
 
---
+++ src/empleados/ListadoEmpleados.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function listadoEmpleados() {
+    return (
+        <div className="container text-center mt-5">
+            <h2>Sistema de Recursos Humanos</h2>
+        </div>
+    )
+}
