12df0cd04532a8d613b9b190c482faf804dbe492
Nefta11 <neftaliarturohernandez@gmail.com>
2024-12-20 14:37:04 -0600

    Agregar axios como dependencia y modificar ListadoEmpleados para cargar empleados desde la API

--- src/empleados/ListadoEmpleados.jsx
+++ src/empleados/ListadoEmpleados.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
-export default function listadoEmpleados() {
+export default function ListadoEmpleados() {
+
+    const urlBase = "http://localhost:8080/rh.app/empleados";
+    const [empleados, setEmpleados] = useState([]);
+
+    useEffect(() => {
+        cargarEmpleados();
+    }, []);
+
+    const cargarEmpleados = async () => {
+        const response = await axios.get(urlBase);
+        console.log("Resultados cargar empleados");
+        console.log(response.data);
+        setEmpleados(response.data);
+    };
+
     return (
-        <div className="container text-center mt-5">
-            <h2>Sistema de Recursos Humanos</h2>
+        <div className="container">
+            <div className="container text-center" style={{ margin: '30px' }}>
+                <h2>Sistema de Recursos Humanos</h2>
+            </div>
+            <table className="table table-striped table-hover align-middle">
+                <thead className='table-dark'>
+                    <tr>
+                        <th scope="col">Id</th>
+                        <th scope="col">Empleado</th>
+                        <th scope="col">Departamento</th>
+                        <th scope="col">Sueldo</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {empleados.map((empleado) => (
+                        <tr key={empleado.id}>
+                            <th scope="row">{empleado.id}</th>
+                            <td>{empleado.nombre}</td>
+                            <td>{empleado.departamento}</td>
+                            <td>{empleado.sueldo}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </div>
-    )
+    );
 }
